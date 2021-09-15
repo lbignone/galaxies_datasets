1c8a0c20666abcd86acfa4913fcd3d17683d7dc9
lbignone <lbignone@gmail.com>
2021-09-15 10:11:43 -0300

    move datasets to subpackage

---
+++ src/galaxies_datasets/datasets/__init__.py
@@ -0,0 +1,5 @@
+"""Datasets."""
+from .eagle import Eagle  # noqa: F401
+from .galaxy_zoo_2 import GalaxyZoo2  # noqa: F401
+from .galaxy_zoo_challenge import GalaxyZooChallenge  # noqa: F401
+from .galaxy_zoo_decals import GalaxyZooDecals  # noqa: F401
