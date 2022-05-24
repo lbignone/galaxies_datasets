0a814cf23eea75516491fd2e76a53d06fcf55272
lbignone <lbignone@gmail.com>
2022-05-23 21:14:23 -0300

    Rename script

--- src/galaxies_datasets/scripts/__init__.py
+++ src/galaxies_datasets/scripts/__init__.py
@@ -3,9 +3,9 @@ import typer
 
 from galaxies_datasets.scripts import documentation
 from galaxies_datasets.scripts import eagle
-from galaxies_datasets.scripts import galaxyzoo3d
+from galaxies_datasets.scripts import galaxy_zoo_3d
 
 app = typer.Typer()
 app.add_typer(eagle.app, name="eagle")
-app.add_typer(galaxyzoo3d.app, name="galaxyzoo3d")
+app.add_typer(galaxy_zoo_3d.app, name="galaxy_zoo3d")
 app.add_typer(documentation.app, name="documentation")
--- src/galaxies_datasets/scripts/galaxyzoo3d/__init__.py
+++
@@ -1,2 +0,0 @@
-"""scripts for Galaxy Zoo 3D."""
-from .download import app  # noqa: F401
